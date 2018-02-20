9b2e9c9404b929f525a6df3d468b15084bf8fb43
Matt Colman <matt.colman@fantasycentre.com>
2018-02-20 14:39:59 +1100

    compile

--- dist/ReactProgressiveList.js
+++ dist/ReactProgressiveList.js
@@ -1,13 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var React = _interopRequireWildcard(_react);
+
+var _times = require('lodash/times');
+
+var _times2 = _interopRequireDefault(_times);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-import * as React from 'react';
-import times from 'lodash/times';
 
 // [MC] Progressive load list items. By default it loads X rows first, then
 // when you scroll to the end of that list it requests to load more (displaying a loader at the end
@@ -129,7 +144,7 @@ var ReactProgressiveList = function (_React$PureComponent) {
           },
           className: className
         },
-        times(numRenderRows, function (i) {
+        (0, _times2.default)(numRenderRows, function (i) {
           return renderItem(i);
         }),
         numRenderRows < rowCount && renderLoader()
@@ -211,6 +226,4 @@ ReactProgressiveList.propTypes = {
   renderLoader: require('prop-types').func,
   useWindowScroll: require('prop-types').bool
 };
-
-
-export default ReactProgressiveList;
+exports.default = ReactProgressiveList;
